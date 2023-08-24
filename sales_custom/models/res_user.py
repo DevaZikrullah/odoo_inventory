bbafe338e2bef1fed9d209c9cd1abd40fc8257c0
DevaZikrullah <zdeva804@gmail.com>
2023-08-24 16:45:56 +0700

    get customer

--- sales_custom/models/res_user.py
+++ sales_custom/models/res_user.py
@@ -3,6 +3,6 @@ from odoo import models, api, http, fields
 
 class resPartner(models.Model):
     _inherit = 'res.partner'
-    #
-    # customer_accurate_id = fields.Char('Accurate ID Customer')
-    # customer_accurate_no = fields.Char()
+
+    customer_accurate_id = fields.Char('Accurate ID Customer')
+    customer_accurate_no = fields.Char()
