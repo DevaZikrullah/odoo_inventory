c91c952983d953ba3e2a99ed7e658ec2ce2f3b20
DevaZikrullah <zdeva804@gmail.com>
2023-08-27 21:50:53 +0700

    modified and add field

--- sales_custom/models/stock.py
+++ sales_custom/models/stock.py
@@ -1,14 +1,39 @@
-from odoo import models, api, http, fields
-from odoo.http import request
+from odoo import models, fields,api
 
 
 class StockInh(models.Model):
-    _inherit = 'stock.picking.type'
+    _inherit = 'stock.picking'
 
-    # item_accurate_id = fields.Char('Accurate ID')
-    # accurate_status = fields.Char()
-    # customer = fields.Char()
-    # no_accurate = fields.Char()
+    address_customer = fields.Char(string='Address Customer',compute='address_cust')
+    vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle')
+    state = fields.Selection(selection_add=[
+        ('delivery', 'Delivery'), ('done',)
+    ])
 
-    def action_accurate(self):
-        return "po"
+    def delivered(self):
+        active_ids = self.env.context.get('active_ids', [])
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Delivered',
+            'res_model': 'delivered.wizard',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'data': active_ids
+            }
+        }
+
+    @api.depends('origin')
+    def address_cust(self):
+        for value in self:
+            address = self.env['sale.order'].search(
+                [('name', '=', value.origin)])
+            value.address_customer = address.accurate_address
+
+    # @api.depends('state')
+    # def vehicle_compute(self):
+    #     if self.state == 'done':
+    #         print(self)
+    #         # self.vehicle_id = None
+
