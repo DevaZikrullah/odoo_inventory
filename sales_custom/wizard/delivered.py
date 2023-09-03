2095964620121db3edbb28fb24c4a61df3a5cd42
DevaZikrullah <zdeva804@gmail.com>
2023-09-03 10:00:10 +0700

    update

--- sales_custom/wizard/delivered.py
+++ sales_custom/wizard/delivered.py
@@ -21,45 +21,62 @@ class Delivered(models.Model):
         for value_id in next_stages_id:
             stock_picking = self.env['stock.picking'].search([('id', '=', value_id)])
             stock_picking.write({
-                'state': 'loading',
-                'picking_type_id': 8
+                'state': 'delivered',
+                'picking_type_id': 7
             })
 
     def _get_record_line(self):
         order_line = []
-        so = []
+        product_data = {}  # Dictionary to store product data
+        all_so_numbers = set()  # To store distinct Sales Order numbers
+
         for active_id in self.env.context.get('data'):
             record = self.env['stock.picking'].search([('id', '=', active_id)])
-            # for item in record.sale_id.id:
-            #     so.append(item)
-            # # print(record.sale_id.id)
             sale_order_line = self.env['sale.order.line'].search([('order_id', '=', record.sale_id.id)])
             for value in sale_order_line:
                 sale_order_name = self.env['sale.order'].search([('id', '=', value.order_id.id)])
-                formatted_data = {
-                    'Product': value.name,
-                    'Qty': value.product_uom_qty,
-                    'SO':sale_order_name.name,
-                    'Customer':sale_order_name.customer
-                }
-                order_line.append(formatted_data)
-        print(order_line)
+                product_id = value.product_id.id
+                product_qty = value.product_uom_qty
+                all_so_numbers.add(sale_order_name.name)  # Collect distinct SO numbers
+
+                if product_id in product_data:
+                    product_data[product_id]['qty'] += product_qty
+                    product_data[product_id]['so'].add(sale_order_name.name)
+                    product_data[product_id]['customer'].add(sale_order_name.customer)
+                else:
+                    product_data[product_id] = {
+                        'name': value.name,
+                        'qty': product_qty,
+                        'so': {sale_order_name.name},
+                        'customer': {sale_order_name.customer}
+                    }
+
+        so_headers = ", ".join([f"SO={so}" for so in all_so_numbers])
 
         table_rows = ""
-        for data in order_line:
+        for product_id, data in product_data.items():
+            qty = data['qty']
+            so_list = ", ".join(data['so'])
+            customer_list = ", ".join(data['customer'])
+
             table_rows += f"""
             <tr>
-                <td>{data['Product']}</td>
-                <td>{data['Qty']}</td>
-                <td>{data['SO']}</td>
-                <td>{data['Customer']}</td>
+                <td>{data['name']}</td>
+                <td>{product_id}</td>
+                <td>{qty}</td>
+                <td>{so_list}</td>
+                <td>{customer_list}</td>
             </tr>
             """
 
         table = f"""
         <table>
             <tr>
+                <th colspan="5">Sales Orders: {so_headers}</th>
+            </tr>
+            <tr>
                 <th>Product</th>
+                <th>Product ID</th>
                 <th>Quantity</th>
                 <th>SO</th>
                 <th>Customer</th>
